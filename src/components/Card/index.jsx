9b71c3ac6e0efe4ed228980c55b80b340e667db0
ruanfox <ruanfox001@gmail.com>
2024-03-21 16:10:15 -0300

    first commit

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,87 +1,57 @@
 import styles from './Card.module.css'
+import PropTypes from 'prop-types';
+import { BiLogoTypescript } from "react-icons/bi";
 import { FaHtml5, FaPhp } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa6";
 import { RiJavascriptFill } from "react-icons/ri";
 import { FaJava } from "react-icons/fa6";
 import { FaArrowAltCircleRight } from "react-icons/fa";
-import { SiPhp } from "react-icons/si";
+import { BsFiletypeScss } from "react-icons/bs";
+
+const iconMap = {
+    HTML: <FaHtml5 />,
+    CSS: <FaCss3Alt />,
+    PHP: <FaPhp />,
+    JavaScript: <RiJavascriptFill />,
+    Java: <FaJava />,
+    TypeScript: <BiLogoTypescript />,
+    SCSS: <BsFiletypeScss />
+};
 
 
-
-// eslint-disable-next-line react/prop-types
-function Card({name, description, html_url,language}){
-    let icons;
-
-    let map = {
-        'Java': <FaJava/> ,
-        'HTML': <FaHtml5/>,
-        'CSS': <FaCss3Alt/>,
-        'JavaScript': <RiJavascriptFill/>,
-        'PHP': <FaPhp/>
+function Card({name, description, language, html_url}){
+    Card.propTypes = {
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        html_url: PropTypes.string.isRequired,
+        language: PropTypes.arrayOf(PropTypes.string).isRequired 
+    };    
+    
+    /*
+    if (language.length >= 0) {
+        icons = language.map(lang => (
+           <span key={lang}>
+               {iconMap[lang]}
+           </span>
+        ));
     }
-
-    switch (language) {
-        case "Java":
-            icons = <FaJava />;
-            break;
-        case "HTML, CSS, JavaScript":
-            icons = (
-                <>
-                    <FaHtml5 />
-                    <FaCss3Alt />
-                    <RiJavascriptFill />
-                </>
-            );
-            break;
-            case "JavaScript, HTML, CSS":
-                icons = (
-                    <>
-                        <FaHtml5 />
-                        <FaCss3Alt />
-                        <RiJavascriptFill />
-                    </>
-                );
-                break;  
-                case "JavaScript, CSS, HTML":
-                    icons = (
-                        <>
-                            <FaHtml5 />
-                            <FaCss3Alt />
-                            <RiJavascriptFill />
-                        </>
-                    );
-                    break;  
-        case "HTML, CSS":
-            icons = (
-                <>
-                    <FaHtml5 />
-                    <FaCss3Alt />
-                </>
-            );
-            break;
-            case "CSS, HTML":
-                icons = (
-                    <>
-                        <FaHtml5 />
-                        <FaCss3Alt />
-                    </>
-                );
-                break;
-        case "PHP":
-            icons = <SiPhp />;
-            break;
-        default:
-            icons = null;
-    }
-
-
+    */
+    console.log(language)
     return(
         <section className={styles.card}>
             <h3>{name}</h3>
             <p>{description}</p>
             <div className={styles.card_footer}>
                 <div className={styles.card_icones}>
-                    {icons}
+                    {
+                        language?.map((item, index) => (
+                            <span key={index}>
+                            {iconMap[item]}{console.log(item)}
+
+                            </span>
+                            
+                        ))
+                    }
                 </div>
                 <a href={html_url} target="_blank" rel="noopenner norefferer noreferrer" className={styles.botao}>
                     <FaArrowAltCircleRight />
--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -3,58 +3,37 @@ import Card from '../../components/Card'
 import styles from './Projetos.module.css'
 function Projetos (){
 
-    const [repositories, setRepositories] = useState([]);
-    const [languages, setLanguages] = useState([]);
-    const [languagesStr, setLanguagesStr] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+  const [languages, setLanguages] = useState([]);
 
-
-    useEffect(() => {
-        const buscarRepositorios = async () => {
-            const response = await fetch('https://api.github.com/users/ruanfox/repos?page=18per_page=50')
-            const data = await response.json()
-            setRepositories(data)
-        }
-        buscarRepositorios()
-    },[])
-
-    useEffect(() => {
-        const fetchLanguages = async (url) => {
-          const response = await fetch(url);
-          const data = await response.json();
-          return Object.keys(data);
-        };
-    
-        const fetchData = async () => {
-          const languagesPromises = repositories.map((repo) => fetchLanguages(repo.languages_url));
-          const languagesData = await Promise.all(languagesPromises);
-          setLanguages(languagesData);
-        };
-    
-        fetchData();
-      }, [repositories]);
-
-    
-      useEffect(() => {
-        languages.forEach((languageArray, index) => {
-          let finalStr = "";
-    
-          languageArray.forEach((language, localIndex) => {
-            finalStr += language;
-    
-            if (localIndex < languageArray.length - 1) {
-              finalStr += ", ";
-            }
-          });
-    
-          setLanguagesStr((prevLanguagesStr) => {
-            const newLanguagesStr = [...prevLanguagesStr];
-            newLanguagesStr[index] = finalStr;
-            return newLanguagesStr;
-          });
-        });
-      }, [languages]);
-    
-
+  useEffect(() => {
+    const buscarRepositorios = async () => {
+      const response = await fetch('https://api.github.com/users/ruanfox/repos?page=18per_page=50')
+      const data = await response.json()
+      setRepositories(data)
+    }
+    buscarRepositorios()
+  },[])
+  
+  useEffect(() => {
+    const fetchLanguages = async (url) => {
+      const response = await fetch(url);
+      const data = await response.json();
+      return Object.keys(data);
+    };
+  
+    const fetchData = async () => {
+      const languagesPromises = repositories.map((repo) => fetchLanguages(repo.languages_url));
+      const languagesData = await Promise.all(languagesPromises);
+      setLanguages(languagesData);
+    };
+  
+    fetchData();
+  }, [repositories]);
+  
+      
+    //console.log(languagesStr)
+    console.log(languages)
     return(
         <section className={styles.projetos}>
             <h2>Projetos</h2> 
@@ -67,7 +46,7 @@ function Projetos (){
                                     key={repo.id} 
                                     name={repo.name} 
                                     description={repo.description} 
-                                    language={languagesStr[index]} 
+                                    language={languages[index]}
                                     html_url={repo.html_url}
                                     />
                             ))
